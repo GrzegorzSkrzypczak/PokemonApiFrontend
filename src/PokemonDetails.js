0d2c1860b9ab704c076590f16ac39dbfa71ed4b3
Grzegorz <rowingteam.grzegorz@gmail.com>
2020-10-24 12:50:44 +0200

    added hooks

--- src/PokemonDetails.js
+++ src/PokemonDetails.js
@@ -1,38 +1,97 @@
-import React from 'react';
-class PokemonDetails extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { pokemonDetails: null }
-    } 
-    componentDidMount() {
-        const name = this.props.match.params.name;
-        fetch('https://pokemon-academi-api-grzegorz.herokuapp.com/pokemons/${name}')
+import React, {useEffect, useState} from 'react';
+import { withRouter } from 'react-router-dom'
+
+const PokemonDetails = (props) => {
+
+    const [pokemonDetails, setPokemonDetails] = useState(null);
+
+    useEffect(()=>{
+        const name = props.match.params.name;
+        fetch(`https://pokemon-academi-api-grzegorz.herokuapp.com/pokemons/${name}`)
         .then(response => response.json())
         .then(jsonResponse => {
             console.log(jsonResponse);
-            this.setState({pokemonDetails: jsonResponse});
+            setPokemonDetails(jsonResponse);
         })
+    }, [])
+
+    const onBackButtonClick = () => {
+        props.history.goBack();
     }
-    renderPokemon = () => {
-        const { imageUrl, name, types, abilities, height, weight } = this.state.pokemonDetails;
+
+    const renderPokemon = () => {
+        const { image, name, type, ability, hight, waight } = pokemonDetails;
         return (
             <div>
                 <h3>{`name: ${name}`}</h3>
-                <h3>{`types: ${types}`}</h3>
-                <h3>{`abilities: ${abilities}`}</h3>
-                <h3>{`height: ${height}`}</h3>
-                <h3>{`weight: ${weight}`}</h3>
-                <img src={imageUrl}/>
+                <h3>{`types: ${type}`}</h3>
+                <h3>{`abilities: ${ability}`}</h3>
+                <h3>{`height: ${hight}`}</h3>
+                <h3>{`weight: ${waight}`}</h3>
+                <img src={image}/>
             </div>
         )
     }
-    render() {
-        return (
-            <div>
-                <h1>Pokemon details</h1>
-                {this.state.pokemonDetails && this.renderPokemon()}
-            </div>
-        )
-    }
+
+    return (
+        <div>
+            <h1>Pokemon details</h1>
+            {/* renderujemy szczegóły jeśli state pokemon details istnieje */}
+            {pokemonDetails && renderPokemon()}
+            {/* renderujemy napis ładowania jeśli state pokemon details nie istnieje */}
+            {!pokemonDetails && <h2>Loading details</h2>}
+            <button onClick={onBackButtonClick}>Back to list</button>
+        </div>
+    )
 }
-export default PokemonDetails;
+
+// class PokemonDetails extends React.Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.state = { pokemonDetails: null }
+//     } 
+
+//     componentDidMount() {
+//         const name = this.props.match.params.name;
+//         fetch(`https://pokemon-academy-api-pbr.herokuapp.com/pokemons/${name}`)
+//         .then(response => response.json())
+//         .then(jsonResponse => {
+//             console.log(jsonResponse);
+//             this.setState({pokemonDetails: jsonResponse});
+//         })
+//     }
+
+//     onBackButtonClick = () => {
+//         this.props.history.goBack();
+//     }
+
+//     renderPokemon = () => {
+//         const { imageUrl, name, types, abilities, height, weight } = this.state.pokemonDetails;
+//         return (
+//             <div>
+//                 <h3>{`name: ${name}`}</h3>
+//                 <h3>{`types: ${types}`}</h3>
+//                 <h3>{`abilities: ${abilities}`}</h3>
+//                 <h3>{`height: ${height}`}</h3>
+//                 <h3>{`weight: ${weight}`}</h3>
+//                 <img src={imageUrl}/>
+//             </div>
+//         )
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Pokemon details</h1>
+//                 {/* renderujemy szczegóły jeśli state pokemon details istnieje */}
+//                 {this.state.pokemonDetails && this.renderPokemon()}
+//                 {/* renderujemy napis ładowania jeśli state pokemon details nie istnieje */}
+//                 {!this.state.pokemonDetails && <h2>Loading details</h2>}
+//                 <button onClick={this.onBackButtonClick}>Back to list</button>
+//             </div>
+//         )
+//     }
+// }
+
+export default withRouter(PokemonDetails);
