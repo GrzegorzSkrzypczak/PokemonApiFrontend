46e825d768e1f7d452c8ea6bcedfd0ffe2bf1843
Grzegorz <rowingteam.grzegorz@gmail.com>
2020-10-24 08:26:26 +0200

    feasture(paa): added pokemon render list - shows pokemons with images, name and lvl

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,14 +1,50 @@
 import React from 'react';
 class PokemonList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {pokemonList: [] };
+    }
+
+
     componentDidMount() {
         fetch('https://pokemon-academi-api-grzegorz.herokuapp.com/pokemons/list')
-        .then(response => {
-            console.log(response);
+        .then(response => response.json())
+        .then(jsonResponse => {
+            console.log(jsonResponse);
+            const results = jsonResponse.results;
+            this.setState({pokemonList: results})
         })
+    }
+
+    renderList = (pokemonList) => {
+        return pokemonList.map((pokemon, index) => {
+            const {name, level, imageUrl } = pokemon;
+            return <tr key={index}> 
+                <td><img src={imageUrl}/></td>
+                <td><p>{name}</p></td>
+                <td><p>{level}</p></td>
+            </tr>
+        })
+    }
+       
+    renderHeader = () => {
+        return <tr>
+            <td><p>Image</p></td>
+            <td><p>Name</p></td>
+            <td><p>Level</p></td>
+        </tr>
+        
     }
     render() {
         return(
-            <h1>Pokemon List!</h1>
+            <div>
+                <h1>Pokemon List: 03:38</h1>
+                <table>
+                    {this.renderHeader()}
+                    {this.renderList(this.state.pokemonList)}
+                </table>
+            </div>
         )
     }
 }
