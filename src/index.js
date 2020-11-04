7ad0d97a53bf94b95bf113b366f607069bdc9ec2
Grzegorz <rowingteam.grzegorz@gmail.com>
2020-11-04 16:53:45 +0100

    feature(paa): changes in index.js for login and register

--- src/index.js
+++ src/index.js
@@ -2,26 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 import PokemonList from './PokemonList';
 import { BrowserRouter, Route } from 'react-router-dom';
 import PokemonDetails from './PokemonDetails';
-
+import RegisterForm from './Register';
+import LoginForm from './Login';
 
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <PokemonList/>
-  // </React.StrictMode>,
   <BrowserRouter>
     <div>
       <Route exact path="/" component={PokemonList}/>
       <Route path="/pokemon/:name" component={PokemonDetails}/>
+      <Route path="/register" component={RegisterForm}/>
+      <Route path="/login" component={LoginForm}/>
     </div>
   </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
